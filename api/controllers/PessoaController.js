fcdde8ff7cd7aba5f00598c5524e1b2331dacd43
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-08-11 16:29:11 -0300

    method get and post for Pessoas created

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,26 @@ class PessoaController {
 			res.status(500).send(err.message);
 		}
 	}
+
+	static async pegaUmaPessoa(req, res) {
+		const { id } = req.params;
+		try {
+			const umaPessoa = await database.Pessoas.findOne({ where: { id: +id } });
+			return res.status(200).send(umaPessoa);
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
+	}
+
+	static async criarPessoa(req, res) {
+		try {
+			const umaPessoa = req.body;
+			await database.Pessoas.create(umaPessoa);
+			return res.status(201).send('Pessoa adicionada com sucesso');
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
+	}
 }
 
 module.exports = PessoaController;
--- api/router/pessoasRoute.js
+++ api/router/pessoasRoute.js
@@ -4,5 +4,7 @@ const PessoaController = require('../controllers/PessoaController.js');
 const route = Router();
 
 route.get('/pessoas', PessoaController.pegarTodasPessoas);
+route.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+route.post('/pessoas', PessoaController.criarPessoa);
 
 module.exports = route;
