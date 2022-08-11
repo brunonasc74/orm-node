c52554d2474a818d741536ecbdd164c78650d0d0
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-08-10 23:57:20 -0300

    index file created

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+app.get('/teste', (_, res) => res.status(200).send({ mensagem: 'oi, api k' }));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`servidor rodando na porta ${port}`));
