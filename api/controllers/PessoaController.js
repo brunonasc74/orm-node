da314b4b2480eab2a2da831f5a826592a03b1013
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-08-11 16:55:22 -0300

    put and delete method created

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,7 +1,7 @@
 const database = require('../models');
 
 class PessoaController {
-	static async pegarTodasPessoas(_, res) {
+	static async pegaTodasPessoas(_, res) {
 		try {
 			const todasPessoas = await database.Pessoas.findAll();
 			return res.status(200).send(todasPessoas);
@@ -20,11 +20,32 @@ class PessoaController {
 		}
 	}
 
-	static async criarPessoa(req, res) {
+	static async criaPessoa(req, res) {
 		try {
 			const umaPessoa = req.body;
 			await database.Pessoas.create(umaPessoa);
 			return res.status(201).send('Pessoa adicionada com sucesso');
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
+	}
+
+	static async editaPessoa(req, res) {
+		try {
+			const { id } = req.params;
+			const novasInfos = req.body;
+			await database.Pessoas.update(novasInfos, { where: { id: +id } });
+			return res.status(200).send('Pessoa atualizada com sucesso');
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
+	}
+
+	static async apagaPessoa(req, res) {
+		try {
+			const { id } = req.params;
+			await database.Pessoas.destroy({ where: { id: +id } });
+			res.status(200).send(`id ${id} apagado com sucesso`);
 		} catch (err) {
 			res.status(500).send(err.message);
 		}
--- api/router/pessoasRoute.js
+++ api/router/pessoasRoute.js
@@ -3,8 +3,10 @@ const PessoaController = require('../controllers/PessoaController.js');
 
 const route = Router();
 
-route.get('/pessoas', PessoaController.pegarTodasPessoas);
+route.get('/pessoas', PessoaController.pegaTodasPessoas);
 route.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
-route.post('/pessoas', PessoaController.criarPessoa);
+route.post('/pessoas', PessoaController.criaPessoa);
+route.put('/pessoas/:id', PessoaController.editaPessoa);
+route.delete('/pessoas/:id', PessoaController.apagaPessoa);
 
 module.exports = route;
